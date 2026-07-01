0fdae39d4b07f9b8e6c1c7a963a42e5968f4cf04
shivank94 <shivanksingh8@outlook.com>
2026-07-01 12:25:50 +0530

    Prediction pipeline

--- src/mlproject1/utils.py
+++ src/mlproject1/utils.py
@@ -6,6 +6,7 @@ import pandas as pd
 from dotenv import load_dotenv
 import pymysql
 
+import dill
 import pickle
 import numpy as np
 
@@ -72,5 +73,13 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
 
         return report
     
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
